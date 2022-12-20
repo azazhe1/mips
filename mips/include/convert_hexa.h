bf71d64d88c4ad66bcddcbe4b9062be0bfb067f8
azazhe1 <106813232+azazhe1@users.noreply.github.com>
2022-12-20 13:47:14 +0100

    Delete mips directory

--- mips/include/convert_hexa.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void gotohexa(char *, char*);
-char tohexa(int);
-int binaryhexa(char *);
-int byte(int);
-void *bit(int, int, char*);
--- mips/include/instructions.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void application(int *tab, char * operateur, int nb_op,char type,int *registre);
